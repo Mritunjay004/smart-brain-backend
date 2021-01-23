fc4e6cf938495291c6e641d151cad9e71b9cab8f
Mritunjay004 <mritunjaygupta004@gmail.com>
2021-01-23 16:04:23 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'vault',
+    database : 'smartbrain'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res) => { res.send('success') })
+
+app.post('/signin', signin.handleSignin(db,bcrypt))
+
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req,res,db) })
+
+app.put('/image', (req,res) => { image.handleImage(req,res,db) })
+app.post('/imageUrl', (req,res) => { image.handleApiCall(req,res) })
+
+app.listen(3000, () => {
+	console.log('app is running');
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
